e61bffef8c915bd7bbe9e8b825e54d6e7e45db5d
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-09 13:31:19 +0300

    add interface app repository

---
+++ src/main/java/org/example/app/repository/AppRepository.java
@@ -0,0 +1,11 @@
+package org.example.app.repository;
+
+import java.util.List;
+
+public interface AppRepository <T>{
+    void create(T odj);
+    List<T> read();
+    T readById(Long id);
+    void update(T odj);
+    void delete(T odj);
+}
